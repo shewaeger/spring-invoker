8fa3afd34097b4d4f2ca1f73df5e1af163cbe650
Shewa Eger <shewaeger@gmail.com>
2019-04-18 17:53:09 +0300

    Prepare to release

--- src/main/java/com/github/shewaeger/websocket/configuration/WebSocketMvcConfig.java
+++ src/main/java/com/github/shewaeger/websocket/configuration/WebSocketMvcConfig.java
@@ -10,8 +10,20 @@ public class WebSocketMvcConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
 
-        registry.addResourceHandler("/ws/**")
-                .addResourceLocations("classpath:/static/frontend/**");
+//        registry.addResourceHandler("/ws/**")
+//                .addResourceLocations("classpath:/static/frontend/**");
+
+        registry.addResourceHandler("/ws/js/**")
+                .addResourceLocations("classpath:/static/frontend/js/");
+
+        registry.addResourceHandler("/ws/css/**")
+                .addResourceLocations("classpath:/static/frontend/css/");
+
+        registry.addResourceHandler("/ws/fonts/**")
+                .addResourceLocations("classpath:/static/frontend/fonts/");
+
+        registry.addResourceHandler("/ws/favicon.ico")
+                .addResourceLocations("classpath:/static/frontend/favicon.ico");
 
         registry.addResourceHandler("/web-socket-tester.html")
                 .addResourceLocations("classpath:/templates/web-socket-tester.html");
